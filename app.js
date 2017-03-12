36f4ed5962f25362dd4fb40d2ba340e219915ec6
iastanley <iasvks.craigslist@gmail.com>
2017-03-12 00:46:40 -0800

    tested real data with betterdoctor API ajax call - worked

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function initMap() {
     event.preventDefault();
     //switch to results view by changing header style
     $('header').removeClass('full-screen');
-    //store location from google maps search
+    //store location from google maps search - hard coded temporarily
     var mapCenter = {lat: 37.7575408, lng: -122.4574279};
     //initialize the map with the user input location
     map = new google.maps.Map(document.getElementById('map'), {
@@ -19,30 +19,30 @@ function initMap() {
       zoom: 8
     });
     //ajax call to betterdoctor API using locaiton as search query and filters
-    // getBetterDoctorData(mapCenter, docDataCallback);
+    getBetterDoctorData(mapCenter, docDataCallback);
 
     //test data - delete for real version
-    getBetterDoctorData(testData);
+    // getBetterDoctorData(testData);
 
   });
 
   //betterdoctor API ajax call handler
   //this is the version for the final code
-  // function getBetterDoctorData(location, callback) {
-  //   //make ajax call to betterdoctor API
-  //   var query = {
-  //     key: '',
-  //     location: location.lat + ',' + location.lng + ',20',
-  //     limit: 20
-  //   }
-  //   $.getJSON(BETTER_DOCTOR_URL, query, callback);
-  // }
+  function getBetterDoctorData(location, callback) {
+    //make ajax call to betterdoctor API
+    var query = {
+      user_key: '21117ecb33b4e4b1650558f7b9657e24',
+      location: location.lat + ',' + location.lng + ',20',
+      limit: 20
+    }
+    $.getJSON(BETTER_DOCTOR_URL, query, callback);
+  }
 
   //testData handler for betterdoctor API data
   //remove from final version
-  function getBetterDoctorData(data) {
-    docDataCallback(data);
-  }
+  // function getBetterDoctorData(data) {
+  //   docDataCallback(data);
+  // }
 
   //callback function for the betterdoctor API call
   function docDataCallback(data){
@@ -86,7 +86,12 @@ function initMap() {
     var uid;
     //build html
     for (var i = 0; i < data.data.length; i++) {
-      imgSrc = data.data[i].profile.image_url ? data.data[i].profile.image_url : 'https://asset3.betterdoctor.com/assets/general_doctor_male.png';
+      try {
+        imgSrc = data.data[i].profile.image_url;
+      } catch(error) {
+        imgSrc = 'https://asset3.betterdoctor.com/assets/general_doctor_male.png';
+      }
+      // imgSrc = data.data[i].profile.image_url ? data.data[i].profile.image_url : 'https://asset3.betterdoctor.com/assets/general_doctor_male.png';
       name = data.data[i].profile.first_name + ' '
             + (data.data[i].profile.middle_name || '') + ' '
             + data.data[i].profile.last_name + ', '
